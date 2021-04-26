4942cb21b349f2e32bb1e2c26f46ac619db66b9f
Regis Santos <regis42santos@gmail.com>
2021-04-26 00:00:25 -0300

    Override __init__ on form.

--- crm/forms.py
+++ crm/forms.py
@@ -4,7 +4,31 @@ from .models import Person
 
 
 class PersonForm(forms.ModelForm):
+    required_css_class = 'required'
 
     class Meta:
         model = Person
-        fields = '__all__'
+        # fields = '__all__'
+        fields = (
+            'first_name',
+            'last_name',
+            'email',
+            'address',
+            'address_number',
+            'complement',
+            'district',
+            'city',
+            'uf',
+            'cep',
+            'country',
+            'cpf',
+            'rg',
+            'cnh',
+            'active',
+        )
+
+    def __init__(self, *args, **kwargs):
+        super(PersonForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+        self.fields['active'].widget.attrs['class'] = None
